539298b5bc46c8f33469bb5d600b210db60d568f
JoseFortunato-coder <jose.11881444@aluno.mg.gov.br>
2026-07-08 11:25:44 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -10,16 +10,21 @@ public class JogoDaVelha  {
         JFrame Janela = new JFrame("Jogo da velha");
         Painel Jogo = new Painel();
         Janela.add(Jogo);
-        Jogo.setPreferredSize(new Dimension(600, 700));
-        Janela.setSize(700,800);
+        Jogo.setPreferredSize(new Dimension(600, 600));
+        Janela.setResizable(false);
+        Janela.setSize(600,600);
         Janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Janela.setLocationRelativeTo(null);
         Janela.setVisible(true);
         Jogo.addMouseListener(new MouseAdapter() {
            @Override
            public void mouseClicked(MouseEvent e) {
-               int linha = e.getY() / 200 ;
-               int coluna = e.getX() / 200;
+               int tamanho = Math.min(Jogo.getWidth(), Jogo.getHeight());
+
+               int casa = tamanho / 3;
+
+               int linha = e.getY() / casa;
+               int coluna = e.getX() / casa;
                Jogo.Jogar(linha, coluna);
                Jogo.repaint();
            }
@@ -41,7 +46,22 @@ class Painel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawLine();
-        g.drawString();
+        int tamanho = Math.min( getWidth(), getHeight());
+        int casas = tamanho / 3;
+        g.drawLine( casas , 0,  casas , tamanho);
+        g.drawLine( casas  * 2, 0,  casas  * 2, tamanho);
+
+        // linhas horizontais
+        g.drawLine(0,  casas , tamanho, casas);
+        g.drawLine(0,  casas  * 2, tamanho, casas * 2);
+        g.setFont(new Font("Arial", Font.BOLD, 80));
+        g.drawString("X", 70, 140);
+        g.drawString("O", 70, 140);
     }
 }
+
+
+
+
+
+
