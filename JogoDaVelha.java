a10622f910ce1d860ce0ad59fe95e0bcd2144217
JoseFortunato-coder <jose.11881444@aluno.mg.gov.br>
2026-07-08 10:54:33 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,29 +1,47 @@
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Scanner;
 import javax.swing.*;
-import java.awt.Dimension;
 
 public class JogoDaVelha  {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         JFrame Janela = new JFrame("Jogo da velha");
-        JPanel Jogo = new JPanel();
+        Painel Jogo = new Painel();
         Janela.add(Jogo);
-        Jogo.setPreferredSize(new Dimension(700, 800)));
+        Jogo.setPreferredSize(new Dimension(600, 700));
         Janela.setSize(700,800);
         Janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Janela.setLocationRelativeTo(null);
         Janela.setVisible(true);
-
         Jogo.addMouseListener(new MouseAdapter() {
            @Override
            public void mouseClicked(MouseEvent e) {
-               super.mouseClicked(e);
-               System.out.print("x");
+               int linha = e.getY() / 200 ;
+               int coluna = e.getX() / 200;
+               Jogo.Jogar(linha, coluna);
+               Jogo.repaint();
            }
        });
             }
+    }
+class Painel extends JPanel {
+
+    char[][] tabuleiro = new char[3][3];
+    char jogador = 'X';
+    char ia = 'O';
+
+    public void Jogar(int linha, int coluna) {
+        if (tabuleiro[linha][coluna] == '\0') {
+            tabuleiro[linha][coluna] = jogador;
+
         }
-
-
-
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawLine();
+        g.drawString();
+    }
+}
